d9b0e19d17601cfbd2def348877265dbf35bf86c
Payal-Khot <159454828+Payal-Khot@users.noreply.github.com>
2024-10-16 20:45:44 +0530

    Add files via upload

---
+++ problem-2.py
@@ -0,0 +1,24 @@
+# WRITE A PROGRAM TO INPUT USRES FIRST NAME AND PRINT ITS LENGTH
+
+'''str = input("enter the name of the user : ")
+print(str)
+print(len(str))
+'''
+# WRITE A PROGRAM TO FIND THE OCCURRENCE OF '$' IN A STRING
+
+# x = "Hi I am $ doller sign my prive is 5$ $$ "
+# print(x.count('$'))
+
+
+str = "this is problem of lecture 2"
+print(str)
+print(type(str))
+count = print(str.count("s"))
+
+word = print(str.find("is"))
+
+len = print(len(str))
+
+print(str.capitalize())
+print(str.endswith("2"))
+new = print(str.replace("2","3"))
