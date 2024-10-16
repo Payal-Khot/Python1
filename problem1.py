bd2b221853c805c94bca4e6bbc50b570b1087907
Payal-Khot <159454828+Payal-Khot@users.noreply.github.com>
2024-10-16 20:45:28 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,33 @@
+'''a=12
+b=2.6
+c="payal"
+print(type(a))
+print(type(b))
+print(type(c))
+'''
+#write a program to input two numbers and print there sum
+
+'''num1 = int(input("Enter the num1 : "))
+num2 = int(input("Enter the num2 : "))
+
+print("sum =",num1+num2)'''
+
+#WRITE A PROGRAM TO INPUT SIDE OF A SQUARE AND PRINT ITS AREA
+
+'''side = int(input("enter side = "))
+
+print("area = ",side**2)'''
+
+#WRITE A PRPGRAM TO INPUT 2 FLOATING NUMBERS AND PRINT THEIR AVERAGE
+
+'''first = float(input("enter first num = "))
+second = float(input("enter second num = "))
+
+print("Average = ",(first+second)/2)'''
+
+#WRITE A PROGRAM TO INPUT 2 INTEGER NUMBERS a AND b PRINT (TRUE) IF a>=b IF NOT PPRINT FALSE
+
+'''a = int(input("enter the first num : "))
+b = int(input("enter the second num : "))
+
+print(a >= b)'''
