7160592accc52fca4ae21cf8761157b91a880529
Payal-Khot <159454828+Payal-Khot@users.noreply.github.com>
2024-10-16 20:46:01 +0530

    Add files via upload

---
+++ problem-3.py
@@ -0,0 +1,34 @@
+# Write a program to ask the user to enter names of their 3 fav movies &
+# store them in list
+# movie = []
+# movie1 = input("enter 1st movie: ")
+# movie2 = input("enter 2nd movie: ")
+# movie3 = input("enter 3rd movie: ")
+# movie.append(movie1)
+# movie.append(movie2)
+# movie.append(movie3)
+# print(movie)
+
+# wap to check if a list contains a plindrome of elements 
+# list = []
+# list.append(input("enter any number : "))
+# list.append(input("enter any number : "))
+# list.append(input("enter any number : "))
+
+# print(list)
+# list_copy=list.copy()
+# list_copy.reverse()
+
+# if(list_copy == list):
+#     print("This is an palindrome")
+# else:
+#     print("not an palindrome")
+
+# wap to count the number of students with the "A" grade in the tuple
+# grade = ("C","D","A","A","B","B","A")
+# print(grade.count("A"))
+
+# store the above values in a list & sort them from "A" to "D"
+# grade = ["C","D","A","A","B","B","A"]
+# grade.sort()
+# print(grade)
